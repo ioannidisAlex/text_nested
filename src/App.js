33a450e6c9d2206e5bcc1bb053477ebe19c19162
ntua-el16038 <ioalexandros@hotmail.com>
2023-11-02 19:47:05 +0200

    contents

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import Document from './components/Document.js'
+
+const DocumentItems = [
+  {
+    "title": "Hydrogen",
+    "content": "Hydrogen is the lightest element in the universe and is essential for life. It is found in water, methane, and other organic compounds."
+  },
+  {
+    "title": "Helium",
+    "content": "Helium is a noble gas that is inert and non-toxic. It is used in balloons, airships, and other applications where a lightweight gas is needed."
+  },
+  {
+    "title": "Lithium",
+    "content": "Lithium is a soft, silvery-white metal that is used in batteries, alloys, and other applications. It is also a mood stabilizer and is used to treat bipolar disorder."
+  }
+];
 
 const App = () => {
   return (
